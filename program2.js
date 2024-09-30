b8e071478a20e0a3905a3350b2b14bdc88907ce0
Sharma's <harshit@Harshits-MacBook-Air.local>
2024-09-30 12:50:07 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -8,11 +8,3 @@ var romanToInt = function(s) {
 
 
 module.exports={romanToInt}
-function romanToInt(s) {
-  // Implement the logic to convert Roman numerals to integers here
-  // You can use a switch statement or a map to store the values of Roman numerals
-
-  return result;
-}
-
-module.exports = { romanToInt };
