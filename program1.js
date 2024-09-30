d45875fee16cd6ad9a32192e63e77e6b85b3467a
Sharma's <harshit@Harshits-MacBook-Air.local>
2024-09-30 12:51:08 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -3,6 +3,14 @@
  * @return {boolean}
  */
 var isValid = function(s) {
+    function isValid(s) {
+        // Implement the logic to check if the input string is valid here
+        // You can use a stack or other data structures to keep track of parentheses
+      
+        return result;
+      }
+      
+      module.exports = { isValid };
     
 };
 
